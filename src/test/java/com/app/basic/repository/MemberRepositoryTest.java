13bace3559b00dda4f31ab746407710f101ae626
jiihyun12 <j22h.h0h@gmail.com>
2025-08-06 21:00:00 +0900

    jpa-주석

--- src/test/java/com/app/basic/repository/MemberRepositoryTest.java
+++ src/test/java/com/app/basic/repository/MemberRepositoryTest.java
@@ -33,48 +33,48 @@ class MemberRepositoryTest {
         member.setMemberAge(17);
         member.setMemberGender(MemberGender.MALE);
     }
-}
-
+//}
+//
+////    @Test
+//    public void memberUpdateTest() {
+//        Optional<Member> updateMember = memberRepository.findById(252L);
+//
+//        updateMember.ifPresent((member) -> {
+//            member.setMemberName("홍길길");
+////            memberRepository.save(member);
+//        });
+//    }
+//
+//    // 수정
 //    @Test
-    public void memberUpdateTest() {
-        Optional<Member> updateMember = memberRepository.findById(252L);
-
-        updateMember.ifPresent((member) -> {
-            member.setMemberName("홍길길");
-//            memberRepository.save(member);
-        });
-    }
-
-    // 수정
-    @Test
-    public void memberUpdateTest() {
-        Optional<Member> optionalMember = memberRepository.findById(252L);
-        optionalMember.ifPresent(member -> {
-            member.setMemberName("홍길길");
-            memberRepository.save(member); // save로 병합
-            log.info("Updated Member: {}", member);
-        });
-    }
-
-    // 조회
-    @Test
-    public void findMemberTest() {
-        Optional<Member> optionalMember = memberRepository.findById(252L);
-        optionalMember.ifPresentOrElse(
-                member -> log.info("조회된 회원: {}", member),
-        );
-    }
-
-    // 삭제
-    @Test
-    public void deleteMemberTest() {
-        Long id = 252L;
-            memberRepository.delete(id);
-            log.info("삭제 완료: ID = {}", id);
-        }
-    }
-
-
-
-
-
+//    public void memberUpdateTest() {
+//        Optional<Member> optionalMember = memberRepository.findById(252L);
+//        optionalMember.ifPresent(member -> {
+//            member.setMemberName("홍길길");
+//            memberRepository.save(member); // save로 병합
+//            log.info("Updated Member: {}", member);
+//        });
+//    }
+//
+//    // 조회
+//    @Test
+//    public void findMemberTest() {
+//        Optional<Member> optionalMember = memberRepository.findById(252L);
+//        optionalMember.ifPresentOrElse(
+//                member -> log.info("조회된 회원: {}", member),
+//        );
+//    }
+//
+//    // 삭제
+//    @Test
+//    public void deleteMemberTest() {
+//        Long id = 252L;
+//            memberRepository.delete(id);
+//            log.info("삭제 완료: ID = {}", id);
+//        }
+//    }
+//
+//
+//
+//
+//
